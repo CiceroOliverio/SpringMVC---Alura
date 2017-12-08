14fae448c46e29bdcb1b12d515b3db61cc0eba93
cicero_oliverio@hotmail.com <Cicero@@sus02>
2017-12-08 16:15:56 -0200

    Primeiro Commit

---
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -0,0 +1,22 @@
+package br.com.casadocodigo.loja.controllers;
+
+import br.com.casadocodigo.loja.models.Produto;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ProdutosController {
+
+    @RequestMapping("/produtos/form")
+    public String form(){
+
+        return "produtos/form";
+    }
+
+    @RequestMapping("/produtos")
+    public String gravar(Produto produto){
+        System.out.println(produto);
+        return "/produtos/ok";
+    }
+
+}
